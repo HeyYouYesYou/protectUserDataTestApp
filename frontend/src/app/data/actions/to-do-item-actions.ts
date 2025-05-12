3d6676077d55543484bdc0de33c1119394d24634
HeyYouYesYou <andriy.yepik@gmail.com>
2025-05-12 09:40:40 +0200

    feat(to-do-item-actions.ts): add server action
    added server action of changing task status:
    toggle isDone status

---
+++ frontend/src/app/data/actions/to-do-item-actions.ts
@@ -0,0 +1,29 @@
+'use server';
+
+import qs from "qs";
+import { revalidatePath } from "next/cache";
+
+import { mutateData } from "../services/mutate-data";
+import { todoItemProps } from "@/app/categories/[documentId]/page";
+
+export const UpdateIsDoneToDos = async (payload: Readonly<todoItemProps>) => {
+    const query = qs.stringify({
+        populate: "*",
+    });
+
+    const newBody = {
+        data: {
+            isDone: payload.isDone
+        }
+    }
+
+    const response = await mutateData({
+        method: "PUT",
+        path: `/api/to-dos/${payload.documentId}?${query}`,
+        body: newBody,
+    });
+
+    revalidatePath(`/categories/${payload.listDocumentId}`);
+
+    return response;
+};
